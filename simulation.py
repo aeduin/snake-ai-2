ceee622b0c1bb3aeb6342bd4048522255dacfa25
aeduin@nanquanu.org <aeduin@nanquanu.org>
2021-06-04 11:20:31 +0200

    working on multi batch

--- simulation.py
+++ simulation.py
@@ -9,7 +9,11 @@ empty_channel = 3
 class World():
     def __init__(self, width: int, height: int, num_worlds: int, device):
         self.device = device
-        self.size = torch.Tensor([width, height]).to(device)
+        self.size = torch.Tensor([num_worlds, width, height]).to(device)
+
+        self.all_worlds_tensor = torch.arange(0, num_worlds, 1, dtype=torch.long, device=device)
+        self.all_width_tensor  = torch.arange(0, width,      1, dtype=torch.long, device=device)
+        self.all_height_tensor = torch.arange(0, height,     1, dtype=torch.long, device=device)
 
         center_x = width // 2
         center_y = width // 2
@@ -20,20 +24,20 @@ class World():
 
         self.space = torch.zeros((num_worlds, num_channels, width, height)).to(device, torch.int16)
 
-        self.space[:, snake_head_channel, self.snake_head_x, self.snake_head_y] = 1
-        self.space[:, snake_channel, self.snake_head_x, self.snake_head_y : self.snake_head_y + 2] = 2
-        # self.space[:, snake_channel, self.snake_head_x, self.snake_head_y + 1] = 1
+        self.space[self.all_worlds_tensor, snake_head_channel, self.snake_head_x, self.snake_head_y] = 1
+        self.space[self.all_worlds_tensor, snake_channel, self.snake_head_x, self.snake_head_y - 1] = 2
+        # self.space[self.all_worlds_tensor, snake_channel, self.snake_head_x, self.snake_head_y + 1] = 1
         
         # self.place_food(1)
 
-        self.space[center_x + 3, center_y, food_channel] = 1
+        self.space[self.all_worlds_tensor, food_channel, center_x + 3, center_y] = 1
 
     def step(self, move_x: torch.Tensor, move_y: torch.Tensor):
         # Calculate the new position of the head
         new_head_x = self.snake_head_x + move_x * (1 - self.dead.to(torch.long))
         new_head_y = self.snake_head_y + move_y * (1 - self.dead.to(torch.long))
         
-        self.dead = self.dead or new_head_x < 0 or new_head_x >= self.size[0] or new_head_y < 0 or new_head_y >= self.size[1] or self.space[new_head_x, new_head_y, snake_channel] > 0
+        self.dead = self.dead or new_head_x < 0 or new_head_x >= self.size[1] or new_head_y < 0 or new_head_y >= self.size[2] or self.space[self.all_worlds_tensor, snake_channel, new_head_x, new_head_y] > 0
 
         # Recalculate the new position of the head in case it died
         new_head_x = self.snake_head_x + move_x * (1 - self.dead.to(torch.long))
@@ -41,13 +45,13 @@ class World():
 
         # print(new_head_x, new_head_y)
 
-        # It there is food at the new location of the head, the snake eats the food
-        eat_food = (self.space[new_head_x, new_head_y, food_channel] == 1).to(torch.long)
+        # If there is food at the new location of the head, the snake eats the food
+        eat_food = (self.space[self.all_worlds_tensor, food_channel, new_head_x, new_head_y] == 1).to(torch.long)
 
         # The time until a piece of the snake disappears decreases by 1 if it has eaten no food, or by 0 if it has
         reduce_time = eat_food - 1
 
-        self.space[self.space[:, :, snake_channel] > 0] += reduce_time
+        self.space[self.space[self.all_worlds_tensor, snake_channel, self.all_width_tensor, self.all_height_tensor] > 0] += reduce_time
         
         # Add 1 to the snake length if it has eaten food
         self.snake_size += eat_food
@@ -106,6 +110,6 @@ if __name__ == "__main__":
     #     for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
     #         w.step(dx, dy)
     for i in range(5):
-        w.step(torch.tensor([1]), torch.tensor([0]))
+        w.step(torch.tensor([1], device=device), torch.tensor([0], device=device))
         print(w)
         input()
